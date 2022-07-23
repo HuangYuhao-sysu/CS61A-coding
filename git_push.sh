148115e0dbf217fd99a3284d7ea19c6c5a659f5e
Huang Yuhao <huangyh76@mail2.sysu.edu.cn>
2022-07-23 23:57:13 +0800

    first commit

---
+++ git_push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# my token = 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
+# git remote set-url origin  https://<your_token>@github.com/<USERNAME>/<REPO>.git
+ git remote set-url origin  https://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@github.com/HuangYuhao-sysu/https://github.com/HuangYuhao-sysu/CS61A-coding.git
